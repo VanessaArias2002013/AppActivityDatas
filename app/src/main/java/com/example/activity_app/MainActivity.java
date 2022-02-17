af6c2cb419045251b2b978f2b419f4f49572e824
VanessaArias2002013 <2002013@utrivieramaya.edu.mx>
2022-02-17 01:02:52 -0500

    Final commit

---
+++ app/src/main/java/com/example/activity_app/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.activity_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText) findViewById(R.id.et1);
+
+    }
+
+    //
+
+    public void Send(View view){
+        Intent i = new Intent(this, MainActivity2.class);
+        i.putExtra("data",et1.getText().toString());
+        startActivity(i);
+
+    }
+}
---
+++ app/src/main/java/com/example/activity_app/MainActivity2.java
@@ -0,0 +1,28 @@
+package com.example.activity_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        tv1 = (TextView) findViewById(R.id.tv1);
+
+        String data = getIntent().getStringExtra("data");
+        tv1.setText("Hello " + data);
+    }
+    //Method back
+    public void back(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
